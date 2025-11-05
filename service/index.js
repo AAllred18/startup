d61ab4ea96b099939675bf59283636fa54007260
AAllred18 <161763678+AAllred18@users.noreply.github.com>
2025-11-05 15:22:14 -0700

    Slowly building out the structure of the backend with necessary files

--- service/index.js
+++ service/index.js
@@ -1,14 +1,23 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the front-end static content hosting
 app.use(express.static('public'));
 
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000',
-    },
-  },
-});
-
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
